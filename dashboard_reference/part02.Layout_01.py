50a29f7e52983865b1c17624d7e4558d18503ba5
seohyeonpark <gane774@naver.com>
2021-09-22 17:01:09 +0900

    Add comments

--- dashboard_reference/part02.Layout_01.py
+++ dashboard_reference/part02.Layout_01.py
@@ -10,28 +10,38 @@ import plotly.express as px
 import pandas as pd
 
 
+# 시트 스타일 선택
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+# 앱 설정
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# 데이터 생성
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
+# 바 그래프 생성
 fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
 
+# 레이아웃 설정
 app.layout = html.Div(children=[
+    # 제목 배치
     html.H1(children='Hello Dash'),
+    # 텍스트 배치
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
+    # 그래프 배치
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=fig      # 보여줄 그래프 설정
     )
 ])
 
+
 if __name__ == '__main__':
+    # 서버 가동
     app.run_server(debug=True)
--- dashboard_reference/part02.Layout_02.py
+++ dashboard_reference/part02.Layout_02.py
@@ -10,45 +10,55 @@ import plotly.express as px
 import pandas as pd
 
 
+# 시트 스타일 선택
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+# 앱 설정
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# 컬러 초기값 설정
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
+# 데이터 생성
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
+# 바 그래프 생성
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
+# 엡데이트 레이아웃 생성
 fig.update_layout(
     plot_bgcolor=colors['background'],
     paper_bgcolor=colors['background'],
     font_color=colors['text']
 )
 
-app.layout = html.Div(style={'backgroundColor': colors['background']},
+# 레이아웃 설정
+app.layout = html.Div(style={'backgroundColor': colors['background']},      # 배경 컬러 설정
                       children=[
+                          # 제목 배치
                           html.H1(
-                              children='Hello Dash',
+                              children='Hello Dash',        # 텍스트
                               style={
-                                  'textAlign': 'centor',
-                                  'color': colors['text']
+                                  'textAlign': 'center',    # 텍스트 위치 설정
+                                  'color': colors['text']   # 텍스트 컬러 설정
                               }
                           ),
+                          # 텍스트 배치
                           html.Div(
                               children='Dash: A web application framework for python',
                               style={
-                                  'textAlign': 'centor',
+                                  'textAlign': 'center',
                                   'color': colors['text']
                               }
                           ),
+                          # 그래프 배치
                           dcc.Graph(
                               id='example-graph-2',
                               figure=fig
@@ -56,4 +66,5 @@ app.layout = html.Div(style={'backgroundColor': colors['background']},
                       ])
 
 if __name__ == '__main__':
+    # 서버 가동
     app.run_server(debug=True)
